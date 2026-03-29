fbe3a1e81c0f002ab7ae9736aa7252ec9b10b513
andergoncalves <andergoncalves@gmail.com>
2026-03-29 15:24:28 -0300

    Finalizacao do Projeto

--- src/swagger.js
+++ src/swagger.js
@@ -6,7 +6,7 @@ const options = {
     openapi: '3.0.0',
     info: {
       title: 'API DevOps - Calculadora',
-      version: '1.0.1',
+      version: '1.0.2',
       description: 'API simples de calculadora com Node.js'
     }
   },
