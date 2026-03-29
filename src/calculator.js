ec4a427da157046a55daf6c45761e5af64c1c5a8
andergoncalves <andergoncalves@gmail.com>
2026-03-29 13:35:20 -0300

    calculadora

--- src/calculator.js
+++ src/calculator.js
@@ -1,10 +1,25 @@
-// Funções básicas da calculadora
-module.exports = {
-  soma: (a, b) => a + b,
-  subtracao: (a, b) => a - b,
-  multiplicacao: (a, b) => a * b,
-  divisao: (a, b) => {
-    if (b === 0) throw new Error('Divisão por zero');
-    return a / b;
+function soma(a, b) {
+  return a + b;
+}
+
+function subtracao(a, b) {
+  return a - b;
+}
+
+function multiplicacao(a, b) {
+  return a * b;
+}
+
+function divisao(a, b) {
+  if (b === 0) {
+    throw new Error('Divisão por zero');
   }
+  return a / b;
+}
+
+module.exports = {
+  soma,
+  subtracao,
+  multiplicacao,
+  divisao
 };
